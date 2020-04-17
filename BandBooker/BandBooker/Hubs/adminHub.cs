d95447f5ffe8f51b5bb539e09ef6256ba99b9910
Prem <prem.rajah@gmail.com>
2020-04-17 12:59:25 +0100

    added SignalR reference and adminHub class

---
+++ BandBooker/BandBooker/Hubs/adminHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandBooker.Hubs
+{
+    public class adminHub : Hub
+    {
+        public async Task SyncMusicianBio(int MusicianId, string Bio)
+        {
+            await Clients.Others.SendAsync("ReceiveSyncMusicianBio", MusicianId, Bio);
+        }
+    }
+}
